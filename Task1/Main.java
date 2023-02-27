7ebdc6dcbad98f678e9b643515f97062c4cd6925
AndresGonzalez23 <72602354+AndresGonzalez23@users.noreply.github.com>
2023-02-27 03:18:12 +0100

    Create .csv working

--- Task1/Main.java
+++ Task1/Main.java
@@ -4,13 +4,21 @@ import java.io.*;
 
 public class Main {
 
+	private static final String COMMA_DELIMITER = ";";
+	private static final String NEW_LINE_SEPARATOR = "\n";
+	private static FileWriter csvWriter;
+	
 	public static void main(String[] args) throws IOException, InterruptedException {
 		
-	String route = "C:/Users/Andrés/eclipse-workspace/Programming Methodology 22-23/src/Imagenes/";
+	String route = "C:/Users/Andres/eclipse-workspace/Programming Methodology 22-23/src/Imagenes/";
+	File csvFile = new File(route + "result.csv");
 	String []listImages = {"320x214", "640x360", "640x427", "1024x1024", "1536x1536"};
+	String[] headers = {"","Greyscale Conversion", "Generate Histogram", "Bubble Conversion", "Quicksort Conversion"};
 	long initTime;
 	long endTime;
-	long difference;
+	csvWriter = new FileWriter(csvFile);
+	
+	writeCSV(headers, csvFile);
 	
 		for (int i = 0; i<listImages.length; i++) {
 			String input = route + listImages[i];
@@ -19,12 +27,13 @@ public class Main {
 			initTime=System.currentTimeMillis();
 			Auxiliar.GenerarImagenGrises(input + ".png", input + "_g.png");
 			endTime=System.currentTimeMillis();
+			String differenceGrey = Long.toString(endTime - initTime);
 			System.out.println("Greyscale image generated");
-			System.out.println("Miliseconds generating greyscale of " + listImages[i] + " = " + (difference=endTime-initTime));
 			
 			initTime=System.currentTimeMillis();
 			int []histogram = Auxiliar.HistogramaImagen(input + "_g.png");
 			endTime=System.currentTimeMillis();
+			String differenceHist = Long.toString(endTime - initTime);
 			System.out.println("Histogram generated");
 			
 			initTime=System.currentTimeMillis();
@@ -34,28 +43,51 @@ public class Main {
 			initTime=System.currentTimeMillis();
 			Auxiliar.GenerarImagenOrdenandoColumnas(input + ".png", input + "_b.png", 0);
 			endTime=System.currentTimeMillis();
+			String differenceBubble = Long.toString(endTime - initTime);
 			System.out.println("Columns ordered with bubble");
 			
 			initTime=System.currentTimeMillis();
 			Auxiliar.GenerarImagenOrdenandoColumnas(input + ".png", input + "_q.png", 1);
 			endTime=System.currentTimeMillis();
+			String differenceQuick = Long.toString(endTime - initTime);
 			System.out.println("Columns ordered with quicksort \n");
 			
+			String[] data = {listImages[i], differenceGrey + " ms" , differenceHist + " ms", differenceBubble + " ms", differenceQuick + " ms"};
+			
+			for(int j = 0; j < data.length; j++) {
+				csvWriter.append(data[j]);
+				if(j != data.length - 1) {
+					csvWriter.append(COMMA_DELIMITER);	
+				}
+			}
+			csvWriter.append(NEW_LINE_SEPARATOR);
+			
 		}
-	
+		csvWriter.close();
 	}
 	
-	public static void transformRelative(String[] listImages, String route) throws IOException, InterruptedException {
-		String input;
-		String output;
-		int [] histogram;
-		for (int i = 0; i<listImages.length; i++) {
-			input = route + listImages[i] + ".png";
-			output = route + listImages[i] + "_g.png";
-			Auxiliar.GenerarImagenGrises(input,output);
-			histogram = Auxiliar.HistogramaImagen(input + "_g.png");
-			Auxiliar.ImprimeHistograma(histogram);
-		}
+	public static void writeCSV(String[] data, File csvFile) throws IOException {
+		csvWriter = new FileWriter(csvFile);
 		
+		for(int i = 0; i < data.length; i++) {
+			csvWriter.append(data[i]);
+			if(i != data.length - 1) {
+				csvWriter.append(COMMA_DELIMITER);	
+			}
+		}
+		csvWriter.append(NEW_LINE_SEPARATOR);
+	}
+	
+	public static void writeNewLineCSV(String[] data, File csvFile) throws IOException{
+		csvWriter = new FileWriter(csvFile);
+		csvWriter.append(NEW_LINE_SEPARATOR);
+		
+		for(int i = 0; i < data.length; i++) {
+			csvWriter.append(data[i]);
+			if(i != data.length - 1) {
+				csvWriter.append(COMMA_DELIMITER);	
+			}
+		}
+		csvWriter.close();
 	}
 }
--- Task1/leer.java
+++ Task1/leer.java
@@ -112,5 +112,3 @@ public class leer{
 	    	return caracter("");
 	}
 }
-
-
