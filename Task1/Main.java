29088ec0b81940032915e052a3073cc533969f60
AndresGonzalez23 <72602354+AndresGonzalez23@users.noreply.github.com>
2023-02-27 17:41:27 +0100

    Program works perfectly
    The program ask the user if he wants ms or ns and create the proper file

--- Task1/Main.java
+++ Task1/Main.java
@@ -1,12 +1,14 @@
 package Task1;
 
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
 	private static final String COMMA_DELIMITER = ";";
 	private static final String NEW_LINE_SEPARATOR = "\n";
 	private static FileWriter csvWriter;
+	private static Scanner scan = new Scanner(System.in);
 	
 	public static void main(String[] args) throws IOException, InterruptedException {
 		
@@ -14,9 +16,12 @@ public class Main {
 	File csvFile = new File(route + "result.csv");
 	String []listImages = {"320x214", "640x360", "640x427", "1024x1024", "1536x1536"};
 	String[] headers = {"","Greyscale Conversion", "Generate Histogram", "Bubble Conversion", "Quicksort Conversion"};
+	String[] elections;
 	long initTime;
 	long endTime;
 	csvWriter = new FileWriter(csvFile);
+	
+	elections = chooseMilliorNano();
 	
 	writeCSV(headers, csvFile);
 	
@@ -24,35 +29,35 @@ public class Main {
 			String input = route + listImages[i];
 			
 			System.out.println("--- Procesing image " + listImages[i] + " ---");
-			initTime=System.currentTimeMillis();
+			initTime=System.currentTimeMillis()*Long.parseLong(elections[1]);
 			Auxiliar.GenerarImagenGrises(input + ".png", input + "_g.png");
-			endTime=System.currentTimeMillis();
+			endTime=System.currentTimeMillis()*Long.parseLong(elections[1]);
 			String differenceGrey = Long.toString(endTime - initTime);
 			System.out.println("Greyscale image generated");
 			
-			initTime=System.currentTimeMillis();
+			initTime=System.currentTimeMillis()*Long.parseLong(elections[1]);
 			int []histogram = Auxiliar.HistogramaImagen(input + "_g.png");
-			endTime=System.currentTimeMillis();
+			endTime=System.currentTimeMillis()*Long.parseLong(elections[1]);
 			String differenceHist = Long.toString(endTime - initTime);
 			System.out.println("Histogram generated");
 			
-			initTime=System.currentTimeMillis();
+			initTime=System.currentTimeMillis()*Long.parseLong(elections[1]);
 			Auxiliar.ImprimeHistograma(histogram);
-			endTime=System.currentTimeMillis();
+			endTime=System.currentTimeMillis()*Long.parseLong(elections[1]);
 			
-			initTime=System.currentTimeMillis();
+			initTime=System.currentTimeMillis()*Long.parseLong(elections[1]);
 			Auxiliar.GenerarImagenOrdenandoColumnas(input + ".png", input + "_b.png", 0);
-			endTime=System.currentTimeMillis();
+			endTime=System.currentTimeMillis()*Long.parseLong(elections[1]);
 			String differenceBubble = Long.toString(endTime - initTime);
 			System.out.println("Columns ordered with bubble");
 			
-			initTime=System.currentTimeMillis();
+			initTime=System.currentTimeMillis()*Long.parseLong(elections[1]);
 			Auxiliar.GenerarImagenOrdenandoColumnas(input + ".png", input + "_q.png", 1);
-			endTime=System.currentTimeMillis();
+			endTime=System.currentTimeMillis()*Long.parseLong(elections[1]);
 			String differenceQuick = Long.toString(endTime - initTime);
 			System.out.println("Columns ordered with quicksort \n");
 			
-			String[] data = {listImages[i], differenceGrey + " ms" , differenceHist + " ms", differenceBubble + " ms", differenceQuick + " ms"};
+			String[] data = {listImages[i], differenceGrey + elections[0] , differenceHist + elections[0], differenceBubble + elections[0], differenceQuick + elections[0]};
 			
 			for(int j = 0; j < data.length; j++) {
 				csvWriter.append(data[j]);
@@ -78,16 +83,36 @@ public class Main {
 		csvWriter.append(NEW_LINE_SEPARATOR);
 	}
 	
-	public static void writeNewLineCSV(String[] data, File csvFile) throws IOException{
-		csvWriter = new FileWriter(csvFile);
-		csvWriter.append(NEW_LINE_SEPARATOR);
-		
-		for(int i = 0; i < data.length; i++) {
-			csvWriter.append(data[i]);
-			if(i != data.length - 1) {
-				csvWriter.append(COMMA_DELIMITER);	
+	public static String[] chooseMilliorNano() {
+		String[] elections = {"",""};
+		int option;
+			System.out.println("Choose if you want to calculate the times in milliseconds of nanoseconds \n 1. Calculate in milliseconds"
+					+ "\n 2. Calculate in nanoseconds\n");
+			while (true) {
+	            System.out.println("Ingresa una opción (1 o 2): ");
+	            String entrada = scan.nextLine();
+
+	            if (entrada.equals("1")) {
+	                option = 1;
+	                break;
+	            } else if (entrada.equals("2")) {
+	                option = 2;
+	                break;
+	            } else {
+	                System.out.println("Entrada inválida. Solo se aceptan 1 o 2 como entrada.");
+	            }
+	        }
+			switch(option) {
+            case 1:
+                elections[0] = "ms";
+                elections[1] = "1";
+                break;
+            case 2:     
+                elections[0] = "ns";
+                elections[1] = "1000000";
+                break;
 			}
-		}
-		csvWriter.close();
+		scan.close();
+		return elections;
 	}
 }
