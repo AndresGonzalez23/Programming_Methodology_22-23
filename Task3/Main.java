5bbe7a7b56c686d0c07800d0e9590fcb9894f37b
AndresGonzalez23 <72602354+AndresGonzalez23@users.noreply.github.com>
2023-04-11 11:16:12 +0200

    End task, need commented code

--- Task3/Main.java
+++ Task3/Main.java
@@ -1,17 +1,17 @@
 package Task3;
 
 import java.util.*;
-
-import Task2.Car;
-
 import java.io.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		String file = "C:/Users/Andres/eclipse-workspace/Programming Methodology 22-23/src/";
+		String file = "C:/Users/Andrés/eclipse-workspace/Programming Methodology 22-23/src/";
 		LinkedList<Cow> cows = new LinkedList<>();
-		LinkedList<Cow> selectedCows = new LinkedList<Cow>();
+		LinkedList<Cow> cowsDescendant = new LinkedList<>();
+		LinkedList<Cow> cowsAscendant = new LinkedList<>();
+		LinkedList<Cow> selectedCowsMilk = new LinkedList<Cow>();
+		LinkedList<Cow> selectedCowsFood = new LinkedList<Cow>();
         String separator = ",";
         int squareMetters, numberOfCowsToSell;
         
@@ -19,17 +19,23 @@ public class Main {
         cows = readFile.readCSV(file, separator);
         numberOfCowsToSell = askNumberOfCowsToSell(cows);
         squareMetters = askMetters();
-        cows = quicksort(cows);
+        cowsDescendant = quicksortAscendant(cows);
         
-        selectedCows = greedyMilkProduction(numberOfCowsToSell, squareMetters, cows);
-        for(Cow cow : selectedCows) {
+        selectedCowsMilk = greedy(numberOfCowsToSell, squareMetters, cowsDescendant);
+        System.out.println("\nSelected Cows for maximize the milk production: ");
+        for(Cow cow : selectedCowsMilk) {
         	System.out.println("code: " + cow.getCode() + " neccesarySpace: " + cow.getNeccesarySpace() + " food: " + cow.getFoodConsumption() + " milk: " + cow.getMilkProduced());
         }
         
-
+        cowsAscendant = quicksortDescendant(cows);
+        selectedCowsFood = greedy(numberOfCowsToSell, squareMetters, cowsAscendant);
+        System.out.println("\nSelected Cows for minimize the food consumption: ");
+        for(Cow cow : selectedCowsFood) {
+        	System.out.println("code: " + cow.getCode() + " neccesarySpace: " + cow.getNeccesarySpace() + " food: " + cow.getFoodConsumption() + " milk: " + cow.getMilkProduced());
+        }
+        
 	}
 
-	
 	public static int askMetters() {
 		Scanner scanner = new Scanner(System.in);
 		int numMetters = 0;
@@ -71,8 +77,7 @@ public class Main {
 		return numberOfCowsToSell;
 	}
 	
-	
-	public static LinkedList<Cow> quicksort(LinkedList<Cow> cows) {
+	public static LinkedList<Cow> quicksortDescendant(LinkedList<Cow> cows) {
 		if(cows.size()<=1) {
 			return cows;
 		}
@@ -82,14 +87,39 @@ public class Main {
 	    
 	    for (int i = 1; i < cows.size(); i++) {
 	    	Cow current = cows.get(i);
-	        if (current.getMilkProduced() >= pivot.getMilkProduced()) {
+	        if (current.getFoodConsumption() <= pivot.getFoodConsumption()) {
 	            less.add(current);
 	        } else {
 	            greater.add(current);
 	        }
 	    }
-	    LinkedList<Cow> sortedLess = quicksort(less);
-	    LinkedList<Cow> sortedGreater = quicksort(greater);
+	    LinkedList<Cow> sortedLess = quicksortDescendant(less);
+	    LinkedList<Cow> sortedGreater = quicksortDescendant(greater);
+	    
+	    sortedLess.add(pivot);
+	    sortedLess.addAll(sortedGreater);
+	    
+		return sortedLess;
+	}
+	
+	public static LinkedList<Cow> quicksortAscendant(LinkedList<Cow> cows) {
+		if(cows.size()<=1) {
+			return cows;
+		}
+		Cow pivot = cows.getFirst();
+		LinkedList<Cow> less = new LinkedList<>();
+	    LinkedList<Cow> greater = new LinkedList<>();
+	    
+	    for (int i = 1; i < cows.size(); i++) {
+	    	Cow current = cows.get(i);
+	        if (current.compareTo(pivot) == -1 || current.compareTo(pivot) == 0) {
+	            less.add(current);
+	        } else {
+	            greater.add(current);
+	        }
+	    }
+	    LinkedList<Cow> sortedLess = quicksortAscendant(less);
+	    LinkedList<Cow> sortedGreater = quicksortAscendant(greater);
 	    
 	    sortedLess.add(pivot);
 	    sortedLess.addAll(sortedGreater);
@@ -97,8 +127,7 @@ public class Main {
 		return sortedLess;
 	}
 		
-	
-	public static LinkedList<Cow> greedyMilkProduction(int numberOfCowsToSell, int squareMetters, LinkedList<Cow> cows){
+	public static LinkedList<Cow> greedy(int numberOfCowsToSell, int squareMetters, LinkedList<Cow> cows){
 		LinkedList<Cow> selectedCows = new LinkedList<Cow>();
 		
 		for(Cow cow : cows) {
